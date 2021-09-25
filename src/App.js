f7bbd5c77f2a6cd0385e77d21f3a149957acd145
Rayhan Sheikh <rsheikh201031@mscse.uiu.ac.bd>
2021-09-26 02:35:03 +0600

    api create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('./dancer.JSON')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  },[])
   return (
     <div className="App">
-  
+      
     </div>
   );
 }
